df6dc305298df9a7a8df9604a421eab20091a7d7
Pixel-ten <gridlinetech10@gmail.com>
2026-02-19 14:27:32 +0530

    Delete web_component/form system/login system directory

--- web_component/form system/login system/basic_login_form.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("login").addEventListener('click', function () {
-    var userName = document.getElementById("username").value;
-    var passWord = document.getElementById("password").value;
-    if (userName == "Admin" && passWord == "12345") {
-        alert("You Are Logged In !")
-    } else {
-        alert("Wrong Password Or Username !!")
-    }
-});
