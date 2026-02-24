679ce17236cfc612b9023e057809afeddb4b967b
Pixel-ten <gridlinetech10@gmail.com>
2026-02-24 10:46:14 +0530

    add web components

---
+++ web_component/navigation system/right_side_multi_nav_mobile_responsive.js
@@ -0,0 +1,17 @@
+var navClick = false;
+document.getElementById("nav_icon").addEventListener("click", function () {
+    var nav = document.getElementById("nav");
+    var ul = document.getElementById("ul");
+    if (navClick === false) {
+        nav.style.display = "flex";
+        ul.style.flexDirection = "column";
+        nav.style.position = "absolute";
+        nav.style.top = "50px";
+        nav.style.right = "-5vw";
+        nav.style.left = "-95vw";
+        navClick = true;
+    } else {
+        nav.style.display = "none";
+        navClick = false;
+    }
+});
